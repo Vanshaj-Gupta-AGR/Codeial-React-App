6eb78c66c6e89d8a1ec278eb421aed72d8f5b72f
Vanshaj Gupta <vanshaj444@gmail.com>
2022-01-21 23:29:52 +0530

    fetching posts

---
+++ src/actions/posts.js
@@ -0,0 +1,22 @@
+import { UPDATE_POSTS } from './actionTypes';
+
+export function fetchPosts() {
+  return (dispatch) => {
+    const url = 'https://aqueous-cliffs-63191.herokuapp.com/api/v1/posts'
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        dispatch(updatePosts(data.posts));
+      });
+  };
+}
+
+export function updatePosts(posts) {
+  return {
+    type: UPDATE_POSTS,
+    posts,
+  };
+}
