34e662179e7b5906153723b4cfff295d8e971561
Vanshaj Gupta <vanshaj444@gmail.com>
2022-01-22 19:14:59 +0530

    login reducers

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,22 +3,37 @@ import React, { Component } from 'react';
 class Login extends Component {
     constructor(props){
         super(props);
-        this.emailInput=React.createRef();
-        this.passwordInput=React.createRef();
+        // this.emailInput=React.createRef();
+        // this.passwordInput=React.createRef();
+
+        this.state={
+            email:'',
+            password:''
+        }
     }
     handleFormSubmit=(e)=>{
         e.preventDefault();
-        console.log(this.emailInput,this.passwordInput);
+        console.log(this.state.email,this.state.password);
+    }
+    handleEmailChange=(e)=>{
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlepasswordChange=(e)=>{
+        this.setState({
+            password: e.target.value
+        })
     }
     render() {
         return (
            <form className='login-form'>
                <span className='login-signup-header'>Log in</span>
                <div className='field'>
-                   <input type="email" placeholder="Email" required ref={this.emailInput}></input>
+                   <input type="email" placeholder="Email" required onChange={this.handleEmailChange} value={this.state.email}></input>
                </div>
                <div className='field'>
-                   <input type="password" placeholder="Password" required ref={this.passwordInput}></input>
+                   <input type="password" placeholder="Password" required onChange={this.handlepasswordChange} value={this.state.password}></input>
                </div>
                <div className='field'>
                   <button onClick={this.handleFormSubmit} >Log In</button>
